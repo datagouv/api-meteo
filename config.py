65167391540471265d6274c94139f03122c79f37
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-08-01 16:03:10 +0200

    add: config

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+## postgres connection ; fill in your own credentials
+PG_USER = os.getenv("POSTGRES_USER")
+PG_PWD = os.getenv("POSTGRES_PASSWORD")
+PG_HOST = os.getenv("POSTGRES_HOST")
+PG_DB = os.getenv("POSTGRES_DB")
+PG_PORT = os.getenv("POSTGRES_PORT")
+PG_SCHEMA = os.getenv("POSTGRES_SCHEMA")
